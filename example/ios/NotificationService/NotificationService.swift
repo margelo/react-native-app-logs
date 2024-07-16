c4540aa0978ba6e3dc4bfa60a33635ee98575f3e
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-07-16 09:39:36 +0200

    chore: add notification service with test logging

--- example/ios/NotificationService/NotificationService.swift
+++ example/ios/NotificationService/NotificationService.swift
@@ -2,17 +2,23 @@
 //  NotificationService.swift
 //  NotificationService
 //
-//  Created by Kiryl Ziusko on 10/07/2024.
+//  Created by Hanno Gödecke on 16.07.24.
 //
 
 import UserNotifications
+import os.log
+import Intents
 
 class NotificationService: UNNotificationServiceExtension {
+  
+  let log = OSLog(subsystem: Bundle.main.bundleIdentifier ?? "applogs.example.NotificationService", category: "NotificationService")
 
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
 
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+      os_log("[NotificationService] didReceive() - received notification", log: log)
+      
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
