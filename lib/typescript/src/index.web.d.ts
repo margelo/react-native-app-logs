017ba17888e5bb67fc1d996362c3fd114a55d817
kirillzyusko <zyusko.kirik@gmail.com>
2024-09-16 15:22:14 +0200

    fix: update build, rename prepare script

--- lib/typescript/src/index.web.d.ts
+++ lib/typescript/src/index.web.d.ts
@@ -2,6 +2,7 @@ declare const AppLogsEvents: {
     registerHandler: () => {
         remove: () => void;
     };
+    configureAppGroupName: () => void;
 };
 export default AppLogsEvents;
 //# sourceMappingURL=index.web.d.ts.map
